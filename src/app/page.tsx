eb4d5c29598779bcf4df177103a6bd051bb61d67
YanGraim <yangraim@gmail.com>
2025-07-01 11:00:10 -0300

    feat: estrutura e estiliza tela de login e cria tela de cadastro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,39 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
+import logoImg from "/public/logo.svg";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <h1 className={styles.teste}>teste teste</h1>
-    </main>
+    <>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="logo da pizza" />
+        <section className={styles.login}>
+          <form>
+            <input
+              type="email"
+              required
+              name="email"
+              placeholder="Digite seu email"
+              className={styles.input}
+            />
+
+            <input
+              type="password"
+              required
+              name="password"
+              placeholder="********"
+              className={styles.input}
+            />
+
+            <button type="submit">Acessar</button>
+          </form>
+          <p className={styles.register}>
+            Ainda não possui uma conta?{" "}
+            <Link href={"/signup"}>Cadastre-se</Link>
+          </p>
+        </section>
+      </div>
+    </>
   );
 }
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,7 @@
+export default function Signup() {
+  return (
+    <main>
+      <h1>Pagina de cadastro</h1>
+    </main>
+  );
+}
