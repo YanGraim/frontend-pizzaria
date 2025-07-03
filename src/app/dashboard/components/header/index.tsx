b2655d806d62b90f97df9f4e335706c0e842cd3a
YanGraim <yangraim@gmail.com>
2025-07-03 11:10:44 -0300

    feat: cria função de logOut

--- src/app/dashboard/components/header/index.tsx
+++ src/app/dashboard/components/header/index.tsx
@@ -1,10 +1,20 @@
+"use client";
 import Image from "next/image";
 import logoImg from "/public/logo.svg";
 import styles from "./styles.module.scss";
 import Link from "next/link";
 import { LogOutIcon } from "lucide-react";
+import { deleteCookie } from "cookies-next";
+import { useRouter } from "next/navigation";
 
 export function Header() {
+  const router = useRouter();
+  async function handleLogOut() {
+    deleteCookie("session", {
+      path: "/",
+    });
+    router.replace("/");
+  }
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
@@ -21,7 +31,7 @@ export function Header() {
         <nav>
           <Link href={"/dashboard/category"}>Categoria</Link>
           <Link href={"/dashboard/product"}>Produto</Link>
-          <form>
+          <form action={handleLogOut}>
             <button type="submit">
               <LogOutIcon size={24} color="#fff" />
             </button>
