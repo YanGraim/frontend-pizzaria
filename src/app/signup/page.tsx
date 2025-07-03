de39be6200c800f96eee96c9b23d1eb749ed2c65
YanGraim <yangraim@gmail.com>
2025-07-03 08:52:03 -0300

    feat: cadastra usuario

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,15 +2,39 @@ import Image from "next/image";
 import styles from "../page.module.scss";
 import logoImg from "/public/logo.svg";
 import Link from "next/link";
+import { api } from "@/services/api";
+import { redirect } from "next/navigation";
 
 export default function Signup() {
+  async function handleRegister(formData: FormData) {
+    "use server"; //função do lado do servidor
+    const name = formData.get("name");
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    if (name === "" || email === "" || password === "") {
+      console.log("Preencha todos os campos");
+      return;
+    }
+
+    try {
+      await api.post("/users", {
+        name: name,
+        email: email,
+        password: password,
+      });
+    } catch (err) {
+      console.log("Erro: ", err);
+    }
+    redirect("/");
+  }
   return (
     <>
       <div className={styles.containerCenter}>
         <Image src={logoImg} alt="logo da pizza" />
         <section className={styles.login}>
           <h1>Criando sua conta</h1>
-          <form>
+          <form action={handleRegister}>
             <input
               type="text"
               required
