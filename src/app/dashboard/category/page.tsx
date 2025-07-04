31f2541ff71e46fa5447fe23a8533f3ee1d01730
YanGraim <yangraim@gmail.com>
2025-07-04 10:54:41 -0300

    feat: cadastra categorias

--- src/app/dashboard/category/page.tsx
+++ src/app/dashboard/category/page.tsx
@@ -1,10 +1,35 @@
+import { api } from "@/services/api";
 import { Button } from "../components/button";
 import styles from "./styles.module.scss";
+import { getCookieServer } from "@/lib/cookieServer";
+import { redirect } from "next/navigation";
 
 export default function Category() {
-  async function handleRegisterCategory() {
+  async function handleRegisterCategory(formData: FormData) {
     "use server";
-    console.log("teste");
+    const name = formData.get("name");
+
+    if (!name) {
+      return;
+    }
+
+    const data = {
+      name: name,
+    };
+
+    const token = await getCookieServer();
+
+    await api
+      .post("/category", data, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .catch((err) => {
+        console.log(err);
+        return;
+      });
+    redirect("/dashboard");
   }
   return (
     <main className={styles.container}>
