e7c5c1c49d028ad60a8d1e66658f70f5ad87e0bd
YanGraim <yangraim@gmail.com>
2025-07-14 09:35:48 -0300

    feat: inicia component formulario

---
+++ src/app/dashboard/product/components/form/index.tsx
@@ -0,0 +1,11 @@
+"use client";
+import styles from "./styles.module.scss";
+
+export function Form() {
+  return (
+    <main className={styles.container}>
+      <h1>Novo produto</h1>
+      <form className={styles.form}></form>
+    </main>
+  );
+}
--- src/app/dashboard/product/page.tsx
+++ src/app/dashboard/product/page.tsx
@@ -1,7 +1,9 @@
+import { Form } from "./components/form";
+
 export default function Product() {
   return (
     <main>
-      <h1>Novo produto</h1>
+      <Form />
     </main>
   );
 }
