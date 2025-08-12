667883db33b5900bea3baefa5babec5ba88634d4
Yan Graim <yangraim@gmail.com>
2025-08-12 09:44:04 -0300

    busca pedidos consumindo api

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,21 @@
+import { api } from "@/services/api";
 import { Orders } from "./components/orders";
+import { getCookieServer } from "@/lib/cookieServer";
+
+async function getOrders() {
+  try {
+    const token = await getCookieServer();
+    const response = await api.get("/orders", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data || [];
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+}
 
 export default function Dashboard() {
   return (
