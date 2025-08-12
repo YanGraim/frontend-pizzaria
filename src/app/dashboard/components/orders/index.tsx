21137723b61767c61c79540722f84a38ef754a54
Yan Graim <yangraim@gmail.com>
2025-08-12 15:20:34 -0300

    feat: cria component modal e estiliza

--- src/app/dashboard/components/orders/index.tsx
+++ src/app/dashboard/components/orders/index.tsx
@@ -1,6 +1,7 @@
 import { RefreshCw } from "lucide-react";
 import styles from "./styles.module.scss";
 import { OrderProps } from "@/lib/order.type";
+import { ModalOrder } from "../modal";
 
 interface Props {
   orders: OrderProps[];
@@ -8,21 +9,24 @@ interface Props {
 
 export function Orders({ orders }: Props) {
   return (
-    <main className={styles.container}>
-      <section className={styles.containerHeader}>
-        <h1>Últimos pedidos</h1>
-        <button>
-          <RefreshCw size={24} color="#3fffa3" />
-        </button>
-      </section>
-      <section className={styles.listOrders}>
-        {orders.map((order) => (
-          <button className={styles.orderItem} key={order.id}>
-            <div className={styles.tag}></div>
-            <span>Mesa {order.table}</span>
+    <>
+      <main className={styles.container}>
+        <section className={styles.containerHeader}>
+          <h1>Últimos pedidos</h1>
+          <button>
+            <RefreshCw size={24} color="#3fffa3" />
           </button>
-        ))}
-      </section>
-    </main>
+        </section>
+        <section className={styles.listOrders}>
+          {orders.map((order) => (
+            <button className={styles.orderItem} key={order.id}>
+              <div className={styles.tag}></div>
+              <span>Mesa {order.table}</span>
+            </button>
+          ))}
+        </section>
+      </main>
+      <ModalOrder />
+    </>
   );
 }
