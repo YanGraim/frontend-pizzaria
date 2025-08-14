1b97f0561d67b245007c1f890d1ce92598b8ae4e
Yan Graim <yangraim@gmail.com>
2025-08-14 17:03:18 -0300

    feat: cria função para o botao de refresh

--- src/app/dashboard/components/orders/index.tsx
+++ src/app/dashboard/components/orders/index.tsx
@@ -5,6 +5,8 @@ import { OrderProps } from "@/lib/order.type";
 import { ModalOrder } from "../modal";
 import { use } from "react";
 import { OrderContext } from "@/providers/order";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 interface Props {
   orders: OrderProps[];
@@ -12,9 +14,15 @@ interface Props {
 
 export function Orders({ orders }: Props) {
   const { isOpen, onRequestOpen } = use(OrderContext);
+  const router = useRouter();
 
   async function handleDetailOrder(orderId: string) {
     await onRequestOpen(orderId);
+  }
+
+  function handleRefresh() {
+    router.refresh();
+    toast.success("Pedidos atualizados com sucesso!");
   }
 
   return (
@@ -22,11 +30,16 @@ export function Orders({ orders }: Props) {
       <main className={styles.container}>
         <section className={styles.containerHeader}>
           <h1>Últimos pedidos</h1>
-          <button>
+          <button onClick={handleRefresh}>
             <RefreshCw size={24} color="#3fffa3" />
           </button>
         </section>
         <section className={styles.listOrders}>
+          {orders.length === 0 && (
+            <span className={styles.emptyItem}>
+              Nenhum pedido aberto no momento...
+            </span>
+          )}
           {orders.map((order) => (
             <button
               className={styles.orderItem}
